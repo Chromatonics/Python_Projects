479255bfca8fdc403a7bd941f5bb91fb87e62f14
Chromatonics <61987650+Chromatonics@users.noreply.github.com>
2020-05-23 10:55:20 -0600

    Add files via upload

---
+++ class1.py
@@ -0,0 +1,14 @@
+class Mineral: 
+      material = 'Solid' 
+      origin = 'Earth' 
+
+class Silicate(Mineral):
+	color = 'Blue'
+	hardness = '7'
+
+class Carbonate(Mineral):
+	chemistry = 'CO3'
+	natural = True
+
+
+
